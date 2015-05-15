26f7dc2a0de4ed30a8d6e9336fdb38a608436cf7
David Sam <chsam27@aol.com>
2015-05-15 11:29:35 -0400

    Added github and new social imgs

--- PersonalWebsite/war/js/angular/controllers/indexController.js
+++ PersonalWebsite/war/js/angular/controllers/indexController.js
@@ -1,6 +1,5 @@
 // this is a controller for the nav
 myApp.controller('indexController', function($scope) {
-	console.log("HELLO from indexcontroller!");
 	$( ".masthead-nav" ).on( "click", "a", function( event ) {
 	    removeClassActive();
 	    $( this ).parent().addClass("active");
